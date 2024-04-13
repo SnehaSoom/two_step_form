e893afbce178bd6200c34c9b87ce28178dfaaf66
ashishMaurya <>
2024-04-13 09:46:24 +0530

    add

--- src/components/FamilyInfoForm.js
+++ src/components/FamilyInfoForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './UserInfoForm.css'
 
 const FamilyInfoForm = ({ onNextStep, onAddFamilyMember, familyMembers, onRemoveFamilyMember, previousPageData }) => {
   const {
@@ -16,18 +17,21 @@ const FamilyInfoForm = ({ onNextStep, onAddFamilyMember, familyMembers, onRemove
     <form onSubmit={handleSubmit(onSubmit)} className="form">
       <h2>Family Information</h2>
       {familyMembers.map((member, index) => (
-        <div key={index}>
-          <input type="text" placeholder="Name" {...register(`familyMembers[${index}].name`, { required: true, pattern: /^[A-Za-z]+$/i })} />
+        <div key={index} className="form-group">
+          <div className='input-box'>
+            <input type="text" placeholder="Name" {...register(`familyMembers[${index}].name`, { required: true, pattern: /^[A-Za-z]+$/i })} />
+            {index > 0 && <button className="remove-button" type="button" onClick={() => onRemoveFamilyMember(index)}>X</button>}
+          </div>
           {errors.familyMembers && errors.familyMembers[index] && errors.familyMembers[index].name && <span className="error">Name is required and must contain only alphabets</span>}
           {/* Add other input fields for family members */}
 
-          
-          {index > 0 && <button type="button" onClick={() => onRemoveFamilyMember(index)}>Remove</button>}
+
+
         </div>
 
-        
+
       ))}
-      <button type="button" onClick={onAddFamilyMember}>Add More Family Member</button>
+      <button type="button" className="add-button" onClick={onAddFamilyMember}>+ Add More Family Member</button>
       <button type="submit" className="next-button">Next</button>
     </form>
   );
